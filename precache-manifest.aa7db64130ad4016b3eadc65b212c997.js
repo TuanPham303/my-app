74da3f2bd832d36eb51e9b9a9e400d773ed08af0
TuanPham303 <phamcongtuan303@gmail.com>
2019-02-27 22:15:39 -0800

    Updates

---
+++ precache-manifest.aa7db64130ad4016b3eadc65b212c997.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "caff8f4450a1e5b1c09b",
+    "url": "/my-app/static/js/runtime~main.caff8f44.js"
+  },
+  {
+    "revision": "7716e666798c6b9db71a",
+    "url": "/my-app/static/js/main.7716e666.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/my-app/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "7716e666798c6b9db71a",
+    "url": "/my-app/static/css/main.d8a4da07.chunk.css"
+  },
+  {
+    "revision": "5fa3f22bb6b40fa798c2b9e6b5e3a387",
+    "url": "/my-app/index.html"
+  }
+];
